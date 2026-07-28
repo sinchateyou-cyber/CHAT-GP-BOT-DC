f5a01c34db851afb2b5dd1f48b873e86e64d2d8f
sinchateyou-cyber <sinchateyou@gmail.com>
2026-07-28 09:39:49 -0300

    Actualizar utilidades.py

--- cogs/utilidades.py
+++ cogs/utilidades.py
@@ -1,18 +1,6 @@
 import discord
 from discord.ext import commands
 
-@commands.command(name="invite")
-async def invite(self, ctx):
-
-    url = discord.utils.oauth_url(
-        self.bot.user.id,
-        permissions=discord.Permissions(administrator=True)
-    )
-
-    await ctx.send(
-        f"🔗 **Invitá a {self.bot.user.name} a tu servidor:**\n{url}"
-    )
-
 class Utilidades(commands.Cog):
 
     def __init__(self, bot):
