8d475aa15828a1a5ddaf918ac510e2828d566a5c
sinchateyou-cyber <sinchateyou@gmail.com>
2026-07-28 11:21:19 -0300

    Actualizar owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from config import OWNER_ID
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
