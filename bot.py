5cdc3c266ca1a4ba60d9b7f6dae1a8eb770ef4ed
sinchateyou-cyber <sinchateyou@gmail.com>
2026-08-15 16:41:35 -0300

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -249,7 +249,7 @@ class MiBot(commands.Bot):
             "cogs.say",
 
             "cogs.mute",
-            "cogs.interacciones",
+            "cogs.animaciones",
 
             # ------------------------------------------------
             # VOICE CREATOR
